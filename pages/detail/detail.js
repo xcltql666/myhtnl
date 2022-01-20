c64917e0d94a3f7139cf71f4211218551f12cce3
xcltql666 <1532481149@qq.com>
2022-01-20 23:11:37 +0800

    add my code

---
+++ pages/detail/detail.js
@@ -0,0 +1,70 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImg:[
+      {src:"https://image.so.com/view?q=%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%9B%BE%E7%89%87&listsrc=sobox&listsign=466a059b5764a74b10c85432b7d46cce&src=360pic_strong&correct=%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%9B%BE%E7%89%87&ancestor=list&cmsid=94e651d9753dff1a06bb515f20661dc6&cmras=6&cn=0&gn=0&kn=0&crn=0&bxn=20&fsn=80&cuben=0&pornn=0&manun=43&adstar=0&clw=275#id=18c9ad57c5e6849e59b67ab1a663a312&currsn=0&ps=45&pc=45"},
+      {src:"https://image.so.com/view?q=%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%9B%BE%E7%89%87&listsrc=sobox&listsign=466a059b5764a74b10c85432b7d46cce&src=360pic_strong&correct=%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%9B%BE%E7%89%87&ancestor=list&cmsid=7749ed2ce5cbccb848d9496eb2c344e2&cmras=6&cn=0&gn=0&kn=0&crn=0&bxn=20&fsn=80&cuben=0&pornn=0&manun=43&adstar=0&clw=275#id=466a059b5764a74b10c85432b7d46cce&currsn=0&ps=45&pc=45"},
+      {src:"https://image.so.com/view?q=%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%9B%BE%E7%89%87&listsrc=sobox&listsign=466a059b5764a74b10c85432b7d46cce&src=360pic_strong&correct=%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%9B%BE%E7%89%87&ancestor=list&cmsid=94e651d9753dff1a06bb515f20661dc6&cmras=6&cn=0&gn=0&kn=0&crn=0&bxn=20&fsn=80&cuben=0&pornn=0&manun=43&adstar=0&clw=275#id=b544823cbc891960df46b1e78d72e54f&currsn=0&ps=45&pc=45"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,73 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImg:[
+      {src:"https://img.alicdn.com/bao/uploaded/i4/2680114444/O1CN017pu14z1ihOmgjpMoS_!!0-item_pic.jpg_300x300q90.jpg"},
+      {src:"https://img.alicdn.com/bao/uploaded/i1/2780997294/O1CN01z6ZO8i23khXM1ez1Z_!!2-item_pic.png_300x300q90.jpg"},
+      {src:"https://p2.ssl.qhimgs1.com/sdr/400__/t019387c468821ea0ec.jpg"}
+    ],
+    newsList:[
+
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
